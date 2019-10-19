bfd9135da86901776a2fb95ac58a7d3e87109980
Leland Curtis <leland.curtis@smithgroup.com>
2019-10-19 15:36:29 -0400

    testing
    not sure

---
+++ daylighting-design-space/database.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+
+namespace daylighting_design_space
+{
+    public class database : GH_Component
+    {
+        /// <summary>
+        /// Initializes a new instance of the MyComponent2 class.
+        /// </summary>
+        public database()
+          : base("Database", "Nickname",
+              "Description",
+              "DDS", "Cat01")
+        {
+        }
+
+
+        /// add other stuff here
+        Dictionary<int, string> myDict = new Dictionary<int, string>();
+
+     
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddBooleanParameter("name", "this is my nickname", "yep yep", GH_ParamAccess.item, true);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+        }
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                //You can add image files to your project resources and access them like this:
+                // return Resources.IconForThisComponent;
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("dec8227d-df90-4d33-91f4-55b2f89c569e"); }
+        }
+    }
+}
