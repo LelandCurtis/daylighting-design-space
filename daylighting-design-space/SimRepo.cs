cfa61ce9218db20c78b778b9f2d64c7e6e121423
Leland Curtis <leland.curtis@smithgroup.com>
2019-10-20 00:54:36 -0400

    Update SimRepo.cs
    added tons of new inputs

--- daylighting-design-space/SimRepo.cs
+++ daylighting-design-space/SimRepo.cs
@@ -42,7 +42,20 @@ namespace DaylightingDesignSpace
             pManager.AddNumberParameter("Data10", "D10", "Input a tree of data, with columns as branches, and rows as lists", GH_ParamAccess.list);
             pManager.AddNumberParameter("Data11", "D11", "Input a tree of data, with columns as branches, and rows as lists", GH_ParamAccess.list);
             pManager.AddNumberParameter("Data12", "D12", "Input a tree of data, with columns as branches, and rows as lists", GH_ParamAccess.list);
-            
+
+            pManager.AddIntegerParameter("Data13", "D13", "Input a tree of data, with columns as branches, and rows as lists", GH_ParamAccess.list);
+            pManager.AddNumberParameter("Data14", "D14", "Input a tree of data, with columns as branches, and rows as lists", GH_ParamAccess.list);
+            pManager.AddNumberParameter("Data15", "D15", "Input a tree of data, with columns as branches, and rows as lists", GH_ParamAccess.list);
+            pManager.AddNumberParameter("Data16", "D16", "Input a tree of data, with columns as branches, and rows as lists", GH_ParamAccess.list);
+            pManager.AddNumberParameter("Data17", "D17", "Input a tree of data, with columns as branches, and rows as lists", GH_ParamAccess.list);
+            pManager.AddNumberParameter("Data18", "D18", "Input a tree of data, with columns as branches, and rows as lists", GH_ParamAccess.list);
+            pManager.AddNumberParameter("Data19", "D19", "Input a tree of data, with columns as branches, and rows as lists", GH_ParamAccess.list);
+            pManager.AddNumberParameter("Dat20", "D20", "Input a tree of data, with columns as branches, and rows as lists", GH_ParamAccess.list);
+            pManager.AddNumberParameter("Data21", "D21", "Input a tree of data, with columns as branches, and rows as lists", GH_ParamAccess.list);
+            pManager.AddNumberParameter("Data22", "D22", "Input a tree of data, with columns as branches, and rows as lists", GH_ParamAccess.list);
+            pManager.AddNumberParameter("Data23", "D23", "Input a tree of data, with columns as branches, and rows as lists", GH_ParamAccess.list);
+            pManager.AddNumberParameter("Data24", "D24", "Input a tree of data, with columns as branches, and rows as lists", GH_ParamAccess.list);
+
             pManager.AddTextParameter("Genome", "G", "Input a genome as properly formated text", GH_ParamAccess.item);
             pManager.AddBooleanParameter("Build Database", "R", "Run 'Build Database' to create a dictionary of Genome objects from the data", GH_ParamAccess.item);
         }
@@ -71,23 +84,35 @@ namespace DaylightingDesignSpace
 
 
             /// initialize the objects
-            List<string> d1 = new List<string>();
-            List<string> d2 = new List<string>();
-            List<string> d3 = new List<string>();
-            List<string> d4 = new List<string>();
-            List<string> d5 = new List<string>();
-            List<string> d6 = new List<string>();
-            List<string> d7 = new List<string>();
-            List<string> d8 = new List<string>();
-            List<string> d9 = new List<string>();
-            List<string> d10 = new List<string>();
-            List<string> d11 = new List<string>();
-            List<string> d12 = new List<string>();
+            List<int> d1 = new List<int>();
+            List<double> d2 = new List<double>();
+            List<double> d3 = new List<double>();
+            List<double> d4 = new List<double>();
+            List<double> d5 = new List<double>();
+            List<double> d6 = new List<double>();
+            List<double> d7 = new List<double>();
+            List<double> d8 = new List<double>();
+            List<double> d9 = new List<double>();
+            List<double> d10 = new List<double>();
+            List<double> d11 = new List<double>();
+            List<double> d12 = new List<double>();
+
+            List<double> d13 = new List<double>();
+            List<double> d14 = new List<double>();
+            List<double> d15 = new List<double>();
+            List<double> d16 = new List<double>();
+            List<double> d17 = new List<double>();
+            List<double> d18 = new List<double>();
+            List<double> d19 = new List<double>();
+            List<double> d20 = new List<double>();
+            List<double> d21 = new List<double>();
+            List<double> d22 = new List<double>();
+            List<double> d23 = new List<double>();
+            List<double> d24 = new List<double>();
 
             String myGenome = "";
-            Genome genome = new Genome();
             Boolean run = new Boolean();
-            Phenome phenome = new Phenome();
+            
 
             DA.GetDataList(0, d1);
             DA.GetDataList(1, d2);
@@ -101,42 +126,56 @@ namespace DaylightingDesignSpace
             DA.GetDataList(9, d10);
             DA.GetDataList(10, d11);
             DA.GetDataList(11, d12);
-            DA.GetData(12, ref myGenome);
-            DA.GetData(13, ref run);
+
+            DA.GetDataList(13, d13);
+            DA.GetDataList(14, d14);
+            DA.GetDataList(15, d15);
+            DA.GetDataList(16, d16);
+            DA.GetDataList(17, d17);
+            DA.GetDataList(18, d18);
+            DA.GetDataList(19, d19);
+            DA.GetDataList(20, d20);
+            DA.GetDataList(21, d21);
+            DA.GetDataList(22, d22);
+            DA.GetDataList(23, d23);
+            DA.GetDataList(24, d24);
+
+            DA.GetData(25, ref myGenome);
+            DA.GetData(26, ref run);
 
 
 
             ///if run = true, create the dictionary
-            if(run == true)
+            if (run == true)
+            {
                 /// clean dictionary
                 gDict.Clear();
                 pDict.Clear();
 
-                
+
                 /// create genomes and phenomes from input data
                 for (int i = 0; i < d1.Count; i++)
-                    ///genome = Genome(d1[i], d2[i], d3[i], d4[i], d5[i], d6[i], d7[i], d8[i], d9[i], d10[i], d11[i], d12[i]);
-
-
-                /// add genomes and phenomes into a dictionary
-                gDict.Add(genome.ToString(), genome);
-                pDict.Add(phenome.ToString(), phenome);
-
-                /// create new lists
-                List<Genome> gList = new List<Genome>();
-                List<Phenome> pList = new List<Phenome>();
-
-                /// cylce through the entire dictionary and copy values (genome  / phenome objects) into output lists
-                foreach (KeyValuePair<string, Genome> kvp in gDict)
                 {
-                    gList.Add(kvp.Value);
-                }
+                    Genome genome = new Genome(d1[i], d2[i], d3[i], d4[i], d5[i], d6[i], d7[i], d8[i], d9[i], d10[i], d11[i], d12[i]);
+                    Phenome phenome = new Phenome(d13[i], d4[i], d15[i], d16[i], d17[i], d18[i], d19[i], d20[i], d21[i], d22[i], d23[i], d24[i]);
 
-                foreach (KeyValuePair<string, Phenome> kvp in pDict)
-                {
-                    pList.Add(kvp.Value);
+                    /// add genomes and phenomes into a dictionary
+                    gDict.Add(genome.ToString(), genome);
+                    pDict.Add(phenome.ToString(), phenome);
                 }
+            }
+
+            /// find output Genome and Phenome based on input genome
+            Genome outGenome = new Genome();
+            Phenome outPhenome = new Phenome();
+            gDict.TryGetValue(myGenome, out outGenome);
+            pDict.TryGetValue(myGenome, out outPhenome);
+
+
+            DA.SetData(0, outGenome);
+            DA.SetData(1, outPhenome);
         }
+
 
         /// <summary>
         /// Provides an Icon for the component.
